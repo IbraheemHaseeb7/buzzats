792b0ba8691c3cfa6cb05be86309cfcbc3398577
Musa Raza <imusaraza@gmail.com>
2023-08-02 23:31:44 +0500

    Suggestion Page Added

---
+++ lib/CustomWidgets/SocietySuggest.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+class SocietySuggest extends StatefulWidget {
+  const SocietySuggest({super.key});
+
+  @override
+  State<SocietySuggest> createState() => _SocietySuggest();
+}
+
+class _SocietySuggest extends State<SocietySuggest> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Row(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        // mainAxisSize: MainAxisSize.max,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(35, 8, 0, 12), 
+            child: Container(
+              margin: EdgeInsets.zero,
+              alignment: Alignment.center,
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              width: screenWidth - 120,
+              height: 123,
+              decoration: BoxDecoration(
+                color: Color(0xff6080a7).withOpacity(0.3),
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(17),
+                border: Border.all(color: Color(0xff6080a7), width: 1),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10, 5, 0, 0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 6, 0, 2),
+                          child: Text(
+                            "IEEE RAS | CUI Lahore",
+                            textAlign: TextAlign.left,
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontStyle: FontStyle.normal,
+                              fontSize: 14,
+                              color: Color(0xffffffff),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 5, 0, 4),
+                          child: Text(
+                            "Education",
+                            textAlign: TextAlign.left,
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontStyle: FontStyle.normal,
+                              fontSize: 14,
+                              color: Color(0xffffffff),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 5, 0, 2),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xFF6080A7),
+                              //shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(8),
+                              // border: Border.all(
+                              //     color: Color(0xff6080a7), width: 1),
+                            ),
+                            padding: EdgeInsets.all(4),
+                            child: Text(
+                              "2K Connections",
+                              textAlign: TextAlign.left,
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14,
+                                color: Color(0xffffffff),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 5, 0, 2),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xFF6080A7),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: EdgeInsets.all(4),
+                            child: Text(
+                              "20 Mutuals",
+                              textAlign: TextAlign.left,
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14,
+                                color: Color(0xffffffff),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                          padding: EdgeInsets.fromLTRB(50, 25, 0, 4),
+                          child: IconButton(
+                            icon: Icon(Icons.arrow_forward_sharp), // Replace with the desired icon
+                          color: Color(0xFF4137BD),
+                          iconSize: 35,
+                          onPressed: (){} ,
+                          ),
+                        ),
+                ],
+              ),
+            ),
+          ),
+        ]);
+  }
+}
---
+++ lib/CustomWidgets/UserSuggest.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+class UserSuggest extends StatefulWidget {
+  const UserSuggest({super.key});
+
+  @override
+  State<UserSuggest> createState() => _UserSuggest();
+}
+
+class _UserSuggest extends State<UserSuggest> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Stack(
+      children: [ 
+        Container(
+        margin: EdgeInsets.zero,
+        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+        width: screenWidth,
+        height: 120,
+        decoration: BoxDecoration(
+          color: Color(0x00474747),
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.zero,
+          border: Border.all(color: Color(0xff6080a7), width: 1),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 3, 20, 0),
+              child: Container(
+                height: 90,
+                width: 70,
+                clipBehavior: Clip.antiAlias,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+                child: Image.asset("lib\\Assets\\musaimage.png",
+                    fit: BoxFit.cover),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 5, 0, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 6, 0, 0),
+                    child: Text(
+                      "Musa Raza",
+                      textAlign: TextAlign.left,
+                      overflow: TextOverflow.clip,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 14,
+                        color: Color(0xffffffff),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                    child: Text(
+                      "Department: Computer Science",
+                      textAlign: TextAlign.left,
+                      overflow: TextOverflow.clip,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 14,
+                        color: Color(0xffffffff),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFF6080A7),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: EdgeInsets.all(4),
+                      child: Text(
+                        "Semester: 5",
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.clip,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 14,
+                          color: Color(0xffffffff),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFF6080A7),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: EdgeInsets.all(4),
+                      child: Text(
+                        "Batch: Fall 2021",
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.clip,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 14,
+                          color: Color(0xffffffff),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      MaterialButton(
+        onPressed: () {
+          // Nevigate to user profile//
+          print("Hi");
+        },
+        minWidth: screenWidth,
+        height: 120,
+      ),
+    ]);
+  }
+}
