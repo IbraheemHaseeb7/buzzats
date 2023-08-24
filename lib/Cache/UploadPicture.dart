1ed47a039877cb7d7f05abdba385621cb70aa77c
Musa Raza <imusaraza@gmail.com>
2023-08-25 04:32:17 +0500

    tweets image & fixes

--- lib/Cache/UploadPicture.dart
+++ lib/Cache/UploadPicture.dart
@@ -22,3 +22,21 @@ Future<String> uploadImageToAzure(Uint8List image) async {
     throw err;
   }
 }
+
+Future<String> uploadSocietyTImageToAzure(Uint8List image) async {
+  try {
+    String fileName = DateTime.now().toString();
+    var storage = AzureStorage.parse(
+        'DefaultEndpointsProtocol=https;AccountName=cuiblob;AccountKey=7HPIx1SCsg48/NL8FTpfgNx9DfUXNM5CvOo+0KEB9BpCdvH4/d33M8qU96G/bwXTEwFRKusqwhlG+AStkhs0tQ==;EndpointSuffix=core.windows.net');
+    String container = "societytweetspictures";
+
+    String? contentType = lookupMimeType(fileName);
+    await storage.putBlob('/$container/$fileName',
+        bodyBytes: image, contentType: contentType, type: BlobType.BlockBlob);
+    return fileName;
+  } on AzureStorageException catch (ex) {
+    throw ex;
+  } catch (err) {
+    throw err;
+  }
+}
